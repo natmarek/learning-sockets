3133087740b0a1c6a81c4f8ef9520de26c5822e4
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-06-19 23:01:54 +0100

    Added broadcasting.

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
 io.on('connection', (socket) => {
     console.log('user connected');
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    })
+    socket.on('chat message', (msg) => {
+        io.emit('message: ' + msg);
+    })
 }) // here we listen on the connection event for incoming sockets
+
+
 
 server.listen(3000, () => {
     console.log('listening on 3000');
